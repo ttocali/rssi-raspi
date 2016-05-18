3214cd3f0b1bfb6d236ca326af4dcceceb84332f
tania <tvt9@cornell.edu>
2016-05-18 10:06:55 -0400

    created table working. need to hange all c.execute() to c.cursor().execute()

--- server/upd-database.py
+++ server/upd-database.py
@@ -1,4 +1,5 @@
-################################################################
+#
+##############################################################
 # Authors: Christine Hwang (ch458) and Tania Tocalini (tvt9)
 # Date:    3-3-2016
 # Lab:     Lab 1
@@ -16,7 +17,8 @@ def formatquotes(text):
  return '"' + str(text) + '"'
 
 def add_to_db( c, location, bssid, rssi, room):
-  c.execute("INSERT INTO " + location + " VALUES ?", (bssid, rssi, room))
+  c.execute("INSERT INTO " + location + " VALUES (?, ?, ?)", (bssid, rssi, room))
+  c.commit()
 
 def get_prints( c, location ):
   return c.execute("SELECT * FROM" + location)
@@ -26,19 +28,24 @@ def get_bssid_prints( c, location, bssid ):
 
 def in_db( c, location, bssid ):
   print(bssid)
-  return c.execute('SELECT EXISTS(SELECT * FROM ' + location + ' WHERE BSSID = (?))', (bssid,))
+  test = c.execute("SELECT EXISTS(SELECT * FROM " + formatquotes(location) + " WHERE BSSID = (?))", (bssid,))
+  print(test)
+  return (test.rowcount > 0)
 
 def upd_db( c, location, bssid, rssi, room):
-  if ( len(in_db( c, location, bssid )) > 0 ):
+  if ( in_db( c, location, bssid )):
     print("BSSID already in DB. Updating values")
     c.execute('UPDATE ' + location + ' SET RSSI = ' + str(rssi) + ', Floor = ' + room + ' WHERE BSSID = ' + bssid)
+    c.commit()
   else:
     print("BSSID not in DB. Adding entry")
-    add_to_db( c, bssid, rssi, room)
+    add_to_db( c, location, bssid, rssi, room)
 
 def create_table( c, location ):
   location = '"' + location + '"'
-  c.execute("CREATE table " + location + " (`BSSID` TEXT UNIQUE, `RSSI` INTEGER, `Floor` TEXT, PRIMARY KEY(BSSID)) WHERE " + location + "NOT EXISTS (SELECT * FROM sqlite_master WHERE type='table'")
+  if (len(c.execute("SELECT * FROM sqlite_master WHERE type='table' AND tbl_name = " + location).fetchall()) == 0):
+    c.execute("CREATE table " + location + " (`BSSID` TEXT UNIQUE, `RSSI` INTEGER, `Floor` TEXT, PRIMARY KEY(BSSID))" )
+  #c.execute("IF SELECT NOT EXISTS(SELECT * FROM sqlite_master WHERE type='table' AND tbl_name = " + location + ") BEGIN CREATE table " + location + " (`BSSID` TEXT UNIQUE, `RSSI` INTEGER, `Floor` TEXT, PRIMARY KEY(BSSID)) END" )
 
 def main():
   conn = sqlite3.connect('pi-fingerprints.db')
